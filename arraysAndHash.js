5e3c678d099d505a9007130d8dfaf1a0e8ed5722
orzoon <orzoonkunwar7.ak@gmail.com>
2023-10-02 22:26:07 +1100

    chagnes

--- arraysAndHash.js
+++ arraysAndHash.js
@@ -196,3 +196,27 @@ function validSudoku__1(sudokuInput) {
 
   return true;
 }
+
+function validISBN10(isbn) {
+  let totalProduct = 0;
+  const ISBN_ARRAY = isbn.split("");
+  // checking the ISBNNO requirement
+  if (
+    ISBN_ARRAY.length < 10 ||
+    ISBN_ARRAY.length > 10 ||
+    (isNaN(ISBN_ARRAY[9]) && ISBN_ARRAY[9] !== "X") ||
+    (!isNaN(ISBN_ARRAY[9]) && (ISBN_ARRAY[9] < 0 || ISBN_ARRAY[9] > 9))
+  )
+    return false;
+  if (ISBN_ARRAY[9] === "X") {
+    ISBN_ARRAY[9] = 10;
+  }
+  // not a number
+  if (!ISBN_ARRAY.every((item) => !isNaN(item))) return false;
+  for (let i = 0; i < 10; i++) {
+    totalProduct = totalProduct + ISBN_ARRAY[i] * (i + 1);
+  }
+
+  if (totalProduct % 11 === 0) return true;
+  else return false;
+}
